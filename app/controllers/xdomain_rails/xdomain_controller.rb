799c2e66bce884dc4880889c528c326b171f3cdc
Lucjan Suski <lucjansuski@gmail.com>
2014-11-11 08:39:25 +0100

    disable sameorigin policy

--- app/controllers/xdomain_rails/xdomain_controller.rb
+++ app/controllers/xdomain_rails/xdomain_controller.rb
@@ -1,8 +1,16 @@
 module XdomainRails
   class XdomainController < ApplicationController
+    after_action :allow_iframe
+
     layout false
 
     def proxy
+    end
+    
+    private
+
+    def allow_iframe
+      response.headers.except! 'X-Frame-Options'
     end
   end
 end
