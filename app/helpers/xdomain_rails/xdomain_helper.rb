488b9ba93154f1dca8a6061ecbfe1650c0d70a62
Lucjan Suski <lucjansuski@gmail.com>
2015-01-12 12:06:53 +0100

    Embed xdomain javascript instead of making request

--- app/helpers/xdomain_rails/xdomain_helper.rb
+++ app/helpers/xdomain_rails/xdomain_helper.rb
@@ -2,15 +2,19 @@ module XdomainRails
   module XdomainHelper
     def xdomain_masters(domains=nil)
       domains = (domains || master_domains).to_json.html_safe
-      javascript_include_tag("xdomain") + javascript_tag("xdomain.masters(#{domains})")
+      xdomain_script_tag + javascript_tag("xdomain.masters(#{domains})")
     end
 
     def xdomain_slaves(domains=nil)
       domains = (domains || slave_domains).to_json.html_safe
-      javascript_include_tag("xdomain") + javascript_tag("xdomain.slaves(#{domains})")
+      xdomain_script_tag + javascript_tag("xdomain.slaves(#{domains})")
     end
 
     private
+
+    def xdomain_script_tag
+      javascript_tag(Rails.application.assets.find_asset("xdomain").source)
+    end
 
     def configuration
       Rails.configuration.xdomain
